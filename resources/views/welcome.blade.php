7294f5ce6d6af0eab0be9c970203ceb00e25a57f
Mal Rice <m.perryrice@gmail.com>
2018-10-22 11:37:50 -0400

    test push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,7 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
+@extends('layouts.master')
 
 
-        <title>Foobooks</title>
-
-    </head>
-    <body>
-
-        <h1>Foobooks</h1>
-
-
-
-    </body>
-</html>
+@section('content')
+    <h1> Welcome to  {{config('app.name')}}</h1>
+    <p> Test upload </p>
+@endsection
