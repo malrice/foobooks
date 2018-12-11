ba75f77986225da139328bdebb1902201303f4c6
Mal Rice <m.perryrice@gmail.com>
2018-12-11 16:48:50 -0500

    foobooks progress

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {    $this->call(AuthorsTableSeeder::class);
         $this->call(TagsTableSeeder::class);
         $this->call(BooksTableSeeder::class);
-        $this->call(BookTagTableSeeder::class);
+         $this->call(BookTagTableSeeder::class);
+
     }
 }
